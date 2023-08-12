ecf01227365f3374952b77e92dbe67135d3263d9
Evaan099 <137773132+Evaan099@users.noreply.github.com>
2023-08-12 16:25:26 +0530

    Update FREE.py

--- FREE.py
+++ FREE.py
@@ -1,13 +1,1 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from REAL import bot.main
-    bot.main()
-elif bit == '32bit':
-    from REAL32 import bot.main
-    bot.main()
+import REAL
